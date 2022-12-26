a7b362abb1502bfcffd8a8a67e5b104a9f88ac95
Chukwudi Nwachukwu <theichux@gmail.com>
2022-12-26 08:51:30 +0100

    improvements

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # async-quart
+To run
+> time curl http://127.0.0.1:5000/max
--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import asyncio
 from asyncio import create_task
 
 from quart import Quart, jsonify
@@ -12,16 +13,24 @@ async def get_data():
     return await gd(1)
 
 
+async def response1():
+    return [await stl(0), await stl(1), await stl(2), await stl(3), await stl(4)]
+
+
+async def response2():
+    # return await asyncio.gather(*[stl(0), stl(1), stl(2), stl(3), stl(4)])
+    return await asyncio.gather(*[stl(_) for _ in range(5)])
+
+
+async def response3():
+    return [await future for future in [create_task(stl(_)) for _ in range(5)]]
+
+
 @app.route("/max")
 async def get_max():
-    # return jsonify({"data": max([await stl(0), await stl(1), await stl(2), await stl(3), await stl(4)])})
-    return jsonify(
-        {
-            "max": max(
-                [await future for future in [create_task(stl(_)) for _ in range(5)]]
-            )
-        }
-    )
+    # return jsonify({"max": max(await response1())})
+    return jsonify({"max": max(await response2())})
+    # return jsonify({"max": max(await response3())})
 
 
 if __name__ == "__main__":
